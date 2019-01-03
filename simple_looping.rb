36f239484fd5de896f0af7ae51b6a6999924be3b
Joe Mullen <mullenjoe13@gmail.com>
2019-01-03 17:51:37 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -3,7 +3,7 @@ def loop_iterator(number_of_times)
 loop do 
   number_of_times = 0
   puts "Welcome to Flatiron School's Web Development Course!"
-    number_of_times += 1
+  number_of_times++
   if number_of_times >= 7 
     break
   end
