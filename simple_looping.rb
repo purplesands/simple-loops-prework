4f54bec73ac603810cd5b99511900c6cc7001edb
Joe Mullen <mullenjoe13@gmail.com>
2019-01-03 17:59:01 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -48,10 +48,5 @@ for number in number_of_times
   end
 end
 end
-  # code your solution here using the "for" keyword
-  # remember that `for` requires a range of numbers. How can we turn our number into a range?
-  
-  phrase = "Welcome to Flatiron School's Web Development Course!"
-  
-end
+
 
